1827d99abd253ecd660efded4fe0ff4c3375b7c3
ivanhack34 <48842215+ivanhack34@users.noreply.github.com>
2023-01-13 16:14:39 -0400

    Exercise of day 16 part 2: 13/1/2023
    Exercises 1 to 5 of this module have been completed.

---
+++ exercise.js
@@ -0,0 +1,56 @@
+//TODO: exercise of day 16 part 2: 13/1/2023
+
+//?1-Use the JavaScript Console to Check the Value of a Variable
+/*
+let a = 5;
+let b = 1;
+a++;
+// Only change code below this line
+
+
+let sumAB = a + b;
+console.log(a); //answer
+*/
+//?2-Understanding the Differences between the freeCodeCamp and Browser Console
+
+/*
+let output = "Get this to show once in the freeCodeCamp console and not at all in the browser console";
+
+console.log(output); //Answer
+
+console.clear(); //Answer
+*/
+
+//?3-Use typeof to Check the Type of a Variable
+
+/*
+let seven = 7;
+let three = "3";
+console.log(seven + three);
+// Only change code below this line
+
+console.log(typeof seven); //answer
+console.log(typeof three); //answer
+*/
+
+//?4-Catch Misspelled Variable and Function Names
+
+/*
+let receivables = 10;
+let payables = 8;
+let netWorkingCapital = receivables - payables; //answer
+console.log(`Net working capital is: ${netWorkingCapital}`);
+*/
+
+//?5-Catch Unclosed Parentheses, Brackets, Braces and Quotes
+
+/*
+let myArray = [1, 2, 3];
+let arraySum = myArray.reduce((previous, current) =>  previous + current);
+console.log(`Sum of array values is: ${arraySum}`);
+*/
+
+//?6-Catch Mixed Usage of Single and Double Quotes
+
+
+
